9bfb610b20323a785a38a2de44c017a64a68141b
Gordon Bateson <gordonbateson@gmail.com>
2019-10-13 07:47:04 +0900

    tool_fixlinks tidy up text in CHANGES.txt

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_fixlinks';
 $plugin->maturity  = MATURITY_BETA;
 $plugin->requires  = 2011120500; // Moodle >= 2.2
-$plugin->version   = 2017061507;
-$plugin->release   = '2017-06-15 (07)';
+$plugin->version   = 2019101208;
+$plugin->release   = '2019-10-12 (08)';
